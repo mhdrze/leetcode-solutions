d34c6ddf002ff5c1133e1cc2e9ce6b519e76e3a5
Mehdi Rezaei <mahdirezaeibu@gmail.com>
2026-09-14 20:44:58 +0330

    Add solution for Two Sum

---
+++ Arrays/0001_Two_Sum.py
@@ -0,0 +1,36 @@
+"""
+Problem:
+1. Two Sum
+
+Difficulty:
+Easy
+
+Topics:
+Array
+
+Approach:
+Check every possible pair of numbers and return the indices
+of the two numbers whose sum equals the target.
+
+Time Complexity:
+O(n^2)
+
+Space Complexity:
+O(1)
+"""
+
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        arr = []
+
+        for i in range(len(nums) - 1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    arr.append(i)
+                    arr.append(j)
+                    return arr
+
+        return arr
